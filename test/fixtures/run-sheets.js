dd6c5c9dd2fba63e87013d2671eda8fe562deda4
denar90 <denysov.artem@gmail.com>
2017-01-15 00:46:59 +0200

    add es6 stuff for sheets

--- test/fixtures/run-sheets.js
+++ test/fixtures/run-sheets.js
@@ -5,7 +5,7 @@ module.exports = {
     options: {
       spreadsheetId: '1Uo4a4KQTaC1FBDPMIJ6xLMmOd2yLyps-6jBC5x4x6I8',
       tableName: 'data',
-      clientSecret: {"installed":{"client_id":"534084362372-2rih05972qlkbihe9fig24huqlm3guid.apps.googleusercontent.com","project_id":"heroic-footing-154820","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"j2OaYgXiHy_EubpAwakT6xgN","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+      clientSecret: {"installed":{"client_id":"22805513477-nojv178bk8kjnft33ugpf3ike8iapr2l.apps.googleusercontent.com","project_id":"pwmetrcis","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"G3g_FQoXuCz3dek_NKGHN89v","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
     }
   }
 };
