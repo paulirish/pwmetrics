6eb1883b9aae09dfcc4b2ff19d8ebfeb7ec5f218
Artem Denysov <denysov.artem@gmail.com>
2017-03-21 23:37:51 +0200

    improve test-runner.sh

--- test-runner.sh
+++ test-runner.sh
@@ -1,5 +1,7 @@
-if [[ $RUN_RECIPES = "1" ]];
-  then yarn run gulp-example;
+#!/usr/bin/env bash
+
+if [[ $RUN_RECIPES = "1" ]]; then
+  yarn run gulp-example;
 else
   npm run build &&
   if [[ $(node -v) =~ ^v7.* ]]; then
