a6821bf53d5e753b2b508f368246caa5a1c42e44
Pedro Silva <ei11061@fe.up.pt>
2017-03-31 20:58:42 +0100

    Tests find median run (#84)
    * Added tests for findMedianRun, modified fixtures mocks to match types/types.ts/Timings interface
    
    * Fixes https://github.com/paulirish/pwmetrics/issues/72 and passes findMedianRun with even runs test
    
    * Fixed array length 2 bug and corrected even run median value.
    
    * remove useless brackets.
    
    * Code review changes.
    
    * Code cleanup and removed median package dependency in yarn lock
    
    * Added additional test case, reduced median function. Implicit pessimistic median value selection with Math.floor

--- test/fixtures/mocks.js
+++ test/fixtures/mocks.js
@@ -18,52 +18,52 @@ exports.metricsResults = [
     'timings': [
       {
         'title': 'First Contentful Paint',
-        'name': 'ttfcp',
-        'value': 598.468,
+        'id': 'ttfcp',
+        'timing': 598.468,
         'color': 'green'
       },
       {
         'title': 'First Meaningful Paint',
-        'name': 'ttfmp',
-        'value': 598.5,
+        'id': 'ttfmp',
+        'timing': 598.5,
         'color': 'green'
       },
       {
         'title': 'Perceptual Speed Index',
-        'name': 'psi',
-        'value': 650,
+        'id': 'psi',
+        'timing': 650,
         'color': 'blue'
       },
       {
         'title': 'First Visual Change',
-        'name': 'fv',
-        'value': 649,
+        'id': 'fv',
+        'timing': 649,
         'color': 'blue'
       },
       {
         'title': 'Visually Complete 100%',
-        'name': 'vc',
-        'value': 649,
+        'id': 'vc',
+        'timing': 649,
         'color': 'blue'
       },
       {
         'title': 'Time to Interactive',
-        'name': 'tti',
-        'value': 635.9,
+        'id': 'tti',
+        'timing': 625.9,
         'color': 'yellow'
       },
       {
         'title': 'Visually Complete 85%',
-        'name': 'vc85',
-        'value': 635.872,
+        'id': 'vc85',
+        'timing': 635.872,
         'color': 'blue'
       }
     ],
     'timestamps': [
       {
         'title': 'Navigation Start',
-        'name': 'navstart',
-        'value': 85880196188
+        'id': 'navstart',
+        'timing': 85880196188
       }
     ],
     'generatedTime': '2017-01-24T23:29:54.465Z',
@@ -75,52 +75,166 @@ exports.metricsResults = [
     'timings': [
       {
         'title': 'First Contentful Paint',
-        'name': 'ttfcp',
-        'value': 598.468,
+        'id': 'ttfcp',
+        'timing': 598.468,
         'color': 'green'
       },
       {
         'title': 'First Meaningful Paint',
-        'name': 'ttfmp',
-        'value': 598.5,
+        'id': 'ttfmp',
+        'timing': 598.5,
         'color': 'green'
       },
       {
         'title': 'Perceptual Speed Index',
-        'name': 'psi',
-        'value': 650,
+        'id': 'psi',
+        'timing': 650,
         'color': 'blue'
       },
       {
         'title': 'First Visual Change',
-        'name': 'fv',
-        'value': 649,
+        'id': 'fv',
+        'timing': 649,
         'color': 'blue'
       },
       {
         'title': 'Visually Complete 100%',
-        'name': 'vc',
-        'value': 649,
+        'id': 'vc',
+        'timing': 649,
         'color': 'blue'
       },
       {
         'title': 'Time to Interactive',
-        'name': 'tti',
-        'value': 635.9,
+        'id': 'tti',
+        'timing': 635.9,
         'color': 'yellow'
       },
       {
         'title': 'Visually Complete 85%',
-        'name': 'vc85',
-        'value': 635.872,
+        'id': 'vc85',
+        'timing': 635.872,
         'color': 'blue'
       }
     ],
     'timestamps': [
       {
         'title': 'Navigation Start',
-        'name': 'navstart',
-        'value': 85880196188
+        'id': 'navstart',
+        'timing': 85880196188
+      }
+    ],
+    'generatedTime': '2017-01-24T23:29:54.465Z',
+    'lighthouseVersion': '1.4.1',
+    'initialUrl': 'http://example.com',
+    'url': 'http://example.com/'
+  },
+  {
+    'timings': [
+      {
+        'title': 'First Contentful Paint',
+        'id': 'ttfcp',
+        'timing': 598.468,
+        'color': 'green'
+      },
+      {
+        'title': 'First Meaningful Paint',
+        'id': 'ttfmp',
+        'timing': 598.5,
+        'color': 'green'
+      },
+      {
+        'title': 'Perceptual Speed Index',
+        'id': 'psi',
+        'timing': 650,
+        'color': 'blue'
+      },
+      {
+        'title': 'First Visual Change',
+        'id': 'fv',
+        'timing': 649,
+        'color': 'blue'
+      },
+      {
+        'title': 'Visually Complete 100%',
+        'id': 'vc',
+        'timing': 649,
+        'color': 'blue'
+      },
+      {
+        'title': 'Time to Interactive',
+        'id': 'tti',
+        'timing': 645.9,
+        'color': 'yellow'
+      },
+      {
+        'title': 'Visually Complete 85%',
+        'id': 'vc85',
+        'timing': 635.872,
+        'color': 'blue'
+      }
+    ],
+    'timestamps': [
+      {
+        'title': 'Navigation Start',
+        'id': 'navstart',
+        'timing': 85880196188
+      }
+    ],
+    'generatedTime': '2017-01-24T23:29:54.465Z',
+    'lighthouseVersion': '1.4.1',
+    'initialUrl': 'http://example.com',
+    'url': 'http://example.com/'
+  },
+  {
+    'timings': [
+      {
+        'title': 'First Contentful Paint',
+        'id': 'ttfcp',
+        'timing': 598.468,
+        'color': 'green'
+      },
+      {
+        'title': 'First Meaningful Paint',
+        'id': 'ttfmp',
+        'timing': 598.5,
+        'color': 'green'
+      },
+      {
+        'title': 'Perceptual Speed Index',
+        'id': 'psi',
+        'timing': 650,
+        'color': 'blue'
+      },
+      {
+        'title': 'First Visual Change',
+        'id': 'fv',
+        'timing': 649,
+        'color': 'blue'
+      },
+      {
+        'title': 'Visually Complete 100%',
+        'id': 'vc',
+        'timing': 649,
+        'color': 'blue'
+      },
+      {
+        'title': 'Time to Interactive',
+        'id': 'tti',
+        'timing': 655.9,
+        'color': 'yellow'
+      },
+      {
+        'title': 'Visually Complete 85%',
+        'id': 'vc85',
+        'timing': 635.872,
+        'color': 'blue'
+      }
+    ],
+    'timestamps': [
+      {
+        'title': 'Navigation Start',
+        'id': 'navstart',
+        'timing': 85880196188
       }
     ],
     'generatedTime': '2017-01-24T23:29:54.465Z',
